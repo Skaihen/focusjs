1292735cb19b792c3dc92b3fa02b3f219f330298
Skaihen <92516247+Skaihen@users.noreply.github.com>
2023-07-29 23:35:31 +0200

    Update Dom.java

--- src/main/java/me/skaihen/Dom.java
+++ src/main/java/me/skaihen/Dom.java
@@ -1,14 +1,30 @@
 package me.skaihen;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Dom {
+    private class AttrMap {
+        HashMap<String, String> data;
+    }
+
     private enum NodeType {
         TEXT, ELEMENT
     }
 
+    private static class ElementData {
+        String tag_name;
+        AttrMap attributes;
+
+    }
+
     public static class Node {
         LinkedList<Node> children;
+        NodeType node_type;
 
+    }
+
+    public Node text(String data) {
+        return new Node();
     }
 }
